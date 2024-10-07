3b61c7ddc0bc290c01b185868bd1c0458042e0cf
Lucifer <gj3372819612@163.com>
2024-10-07 13:47:03 +0800

    add struct event_base

--- include/event/event_internal.h
+++ include/event/event_internal.h
@@ -10,7 +10,7 @@ extern "C" {
 #include <time.h>
 #include <sys/queue.h>
 
-struct lu_event_op{
+struct event_op{
     int Placeholder; //TODO
     // int (*evsel_init)(struct lu_event_base *base);
     // void (*evsel_cleanup)(struct lu_event_base *base);
@@ -19,16 +19,27 @@ struct lu_event_op{
 };
 
 struct event_changelist{
-    int paser;//TODO
+    int paser;
 };
 
-struct lu_event_base{
-    const struct lu_event_op * evsel;
-    void*                      ev_base;
-    struct event_changelist    changelist;
+struct event_base{
+    const struct lu_event_op*   evsel;
+    void*                       ev_base;
+    struct event_changelist     changelist;
+    const struct lu_event_op*   evsigsel;               //用于处理信号   
+    struct ev_sig_info*         sigev;                  //用于实现信号处理通用代码的数据。
 
-    const struct lu_event_op * evsigsel;
-
+    int                         virtual_event_count;    //虚拟事件计数
+    int                         virtual_event_max;      //虚拟事件最大值
+    int                         event_count;            //事件计数 (已经添加到event_base中的事件)
+    int                         event_max;              //事件最大值
+	int                         event_count_active;     //用于表示当前活动事件数量。
+	int                         event_count_active_max; //用于表示最大活动事件数量。
+	int                         event_gotterm;          //用于表示是否应该在处理完事件后终止循环。
+	int                         event_break;            //用于表示是否应该立即终止循环。    
+	int                         event_continue;         //用于表示是否应该在处理完事件后继续执行循环。   
+	int                         event_running_priority; //用于表示当前正在运行的事件优先级。    
+	int                         running_loop;           //用于表示是否正在运行event_base_loop函数，以防止重入调用。
 };
 
 

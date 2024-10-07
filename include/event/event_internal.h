103387a1e6a21cc0130f79c31152c958c30d07de
Lucifer <gj3372819612@163.com>
2024-10-07 14:56:01 +0800

    test

--- include/event/event_internal.h
+++ include/event/event_internal.h
@@ -18,6 +18,9 @@ struct event_op{
     // int (*evsel_del)(struct lu_event_base *base, struct lu_event *ev);
 };
 
+struct event{
+    int paser;
+};
 struct event_changelist{
     int paser;
 };
@@ -44,6 +47,17 @@ struct min_heap       {
 struct evutil_monotonic_timer{
   int paser;  
 };
+
+enum  event_base_config_flag{
+
+};
+
+struct evutil_weakrand_state{
+    int paser;      
+
+};
+
+typedef int evutil_socket_t ;
 
 struct event_base{
     const struct lu_event_op*   evsel;                  //指向后端数据的指针，用于描述event_base端的数据
@@ -97,6 +111,69 @@ struct event_base{
     struct evutil_monotonic_timer monotonic_timer;//用于实现单调时钟
     /** Difference between internal time (maybe from clock_gettime) and  gettimeofday. */
     struct timeval              tv_clock_diff;
+
+    /** Second in which we last updated tv_clock_diff, in monotonic time. */
+	time_t last_updated_clock_diff;             //用于存储上次更新tv_clock_diff时的单调时间。
+
+#ifndef EVENT__DISABLE_THREAD_SUPPORT    
+    /* threading support */
+    /** The thread currently running the event_loop for this base */
+	unsigned long th_owner_id;
+	/** A lock to prevent conflicting accesses to this event_base */
+	void *th_base_lock;
+	/** A condition that gets signalled when we're done processing an
+	 * event with waiters on it. */
+	void *current_event_cond;
+	/** Number of threads blocking on current_event_cond. */
+	int current_event_waiters;
+#endif
+    /** The event whose callback is executing right now */
+	struct event_callback *current_event;
+#ifdef _WIN32
+	/** IOCP support structure, if IOCP is enabled. */
+    /**：一个指向`event_iocp_port`结构体的指针，如果在Windows平台上使用IOCP（I/O Completion Port）作为事件驱动后端，则使用此字段。
+*/
+	struct event_iocp_port *iocp;
+#endif    
+
+	/** Flags that this base was configured with */
+    //`flags`：一个枚举类型，用于表示`event_base`的配置标志。
+    
+	enum event_base_config_flag flags;
+	
+    //，用于表示最大调度时间。
+	struct timeval max_dispatch_time;
+
+    int max_dispatch_callbacks;//用于表示最大调度回调数量。
+	int limit_callbacks_after_prio;//用于表示在达到特定优先级后限制回调数量。
+
+	/* Notify main thread to wake up break, etc. */
+	/** True if the base already has a pending notify, and we don't need
+	 * to add any more. */
+	int is_notify_pending;//用于表示`event_base`是否已经有一个待处理的通知。
+	/** A socketpair used by some th_notify functions to wake up the main
+	 * thread. */
+    
+    //用于free base
+	evutil_socket_t th_notify_fd[2];//一个套接字对，用于在另一个线程中唤醒主线程。
+	/** An event used by some th_notify functions to wake up the main
+	 * thread. */
+    
+	struct event th_notify;//用于在主线程中唤醒其他线程。
+
+	/** A function used to wake up the main thread from another thread. */
+    /**`th_notify`：一个`event`结构体，用于在主线程中唤醒其他线程。*/
+	int (*th_notify_fn)(struct event_base *base);
+
+	/** Saved seed for weak random number generator. Some backends use
+	 * this to produce fairness among sockets. Protected by th_base_lock. */
+    //一个`evutil_weakrand_state`结构体，用于存储弱随机数生成器的种子。
+	struct evutil_weakrand_state weakrand_seed;
+
+	/** List of event_onces that have not yet fired. */
+    /**`once_events`：一个`LIST_HEAD`结构体，用于存储尚未触发的事件。*/
+	LIST_HEAD(once_event_list, event_once) once_events;
+    
 };
 
 

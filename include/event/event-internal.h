0fd17ea7576ed1e41f27000e0cbf11004435d4e7
Lucifer <gj3372819612@163.com>
2024-12-08 00:46:18 +0800

    cache changed

--- include/event/event-internal.h
+++ include/event/event-internal.h
@@ -11,20 +11,34 @@ extern "C" {
 #include <sys/queue.h>
 #include "util.h"
 
-#define PASER (int paser)
-
-struct event_op{
-    int paser;
-};
+#define PASER int paser
 
 
 
-struct event_base{
-    //用于描述这个event_base端
-    const struct event_op *  evsel;
+typedef struct lu_event_base_s{
+    const struct lu_event_operate_t* evsel_op;
+    void*                            evbase;
+}lu_event_base_t;
 
-    void*                   _evbase; 
-};
+
+ 
+typedef struct lu_event_operate_s{
+    // name
+    const char*     name;
+
+    /**
+     * return : 返回一个新创建的结构体
+     *          返回的指针将由event_init()储存在event_base_s.evbase字段中;
+     *          如果初始化失败则返回NULL
+     */
+    void* (*init)(struct lu_event_base_s *);
+
+}event_operate_t;
+
+
+
+
+ 
 
 
 

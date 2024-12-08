b7d3cc09f44762cbcfc6d051f8395b3b472b95f9
Lucifer <gj3372819612@163.com>
2024-12-08 23:37:49 +0800

    update

--- include/event-internal.h
+++ include/event-internal.h
@@ -7,10 +7,9 @@ extern "C" {
 #endif  //__cplusplus
 
 
-#include <time.h>
 #include <sys/queue.h>
 #include "util.h"
-
+#include <sys/time.h>
  
 
 
@@ -19,32 +18,42 @@ typedef struct lu_event_operate_s lu_event_operate_t;
 //typedef struct lu_event_base_s    lu_event_base_t;
 
 
-// 定义 lu_event_base_s 结构体
+ 
 typedef struct lu_event_base_s {
-    // 引用 lu_event_operate_t 类型的指针
-    const lu_event_operate_t* evsel_op;  
+    // 引用 lu_event_operate_t 类型的指针 
+    const struct lu_event_operate_s* evsel_op;  
     // 操作lu_event_base_s
     void* evbase;
 } lu_event_base_t;
 
-// 定义 lu_event_operate_s 结构体
+ 
 typedef struct lu_event_operate_s {
     const char* name;
 
     /**
-     * 返回值：  返回一个新创建的结构体指针;返回的指针将由 event_init() 储存在 event_base_s.evbase 字段中；
+     * return : 返回一个新创建的结构体指针;返回的指针将由 event_init() 储存在 event_base_s.evbase 字段中；
      *          如果初始化失败则返回 NULL。
      */
     void* (*init)(lu_event_base_t*);  // 这里的类型已变为 lu_event_base_t*
 } lu_event_operate_t;
 
 
+typedef struct lu_event_config_entry_s {
+    TAILQ_ENTRY(lu_event_config_entry_s) next;
+    const char *avoid_method;
+} lu_event_config_entry_t;
+
 
 /** Internal structure: describes the configuration we want for an event_base
  * that we're about to allocate. */
 typedef struct lu_event_config_s {
-    int flags;
-    TAILQ_HEAD(lu_event_config_q, lu_event_config_s) entries;
+    //定义一个双向链表的头部。它将被用作存储lu_event_config_entry_s类型的结构体的链表
+    TAILQ_HEAD(lu_event_config_q, lu_event_config_entry_s) entries;
+    int n_cpus_hint;
+
+    //指定最大分派间隔的时间值。
+	struct timeval max_dispatch_interval;
+
 }lu_event_config_t;
  
 
--- include/event.h
+++ include/event.h
@@ -3,7 +3,7 @@
 
 #include "event-internal.h"
 
-// lu_event_base_t * lu_event_base_new(void);
+lu_event_base_t  * lu_event_base_new(void);
 
 
 #endif  //LU_EVENT_H

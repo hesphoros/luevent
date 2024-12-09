13bf0c3ab47ef1c88e43e46489f8e985297cb9b1
Lucifer <gj3372819612@163.com>
2024-12-09 23:44:31 +0800

    update mm-internal.h

--- include/lu_mm-internal.h
+++ include/lu_mm-internal.h
@@ -6,13 +6,15 @@
 extern "C" {
 #endif
 
+#ifndef LU_EVENT__DISABLE_CUSTOM_MM_REPLACEMENT
 #define mm_malloc(sz) 			lu_event_mm_malloc_(sz)
 #define mm_calloc(count, size) 	lu_event_mm_calloc_((count), (size))
 #define mm_strdup(s) 			lu_event_mm_strdup_(s)
 #define mm_realloc(p, sz) 		lu_event_mm_realloc_((p), (sz))
 #define mm_free(p) 				lu_event_mm_free_(p)
 
-#ifdef  LU_EVENT__DISABLE_CUSTOM_MM_REPLACEMENT 
+#else
+// If custom memory management is disabled, use malloc, calloc, etc. from the standard library
 #define mm_malloc(sz) malloc(sz)
 #define mm_calloc(n, sz) calloc((n), (sz))
 #define mm_strdup(s) strdup(s)

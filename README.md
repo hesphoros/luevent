9e511de110ae9012237a6490e135e7519d52714b
Lucifer <gj3372819612@163.com>
2024-12-14 00:05:25 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Development log
+
 # Monday 9 Dec 2024
 1.  封装lu_mm-internal.h 头文件
     LU_EVENT__DISABLE_CUSTOM_MM_REPLACEMENT 宏 
--- src/lu_util.c
+++ src/lu_util.c
@@ -17,6 +17,7 @@ int lu_evutil_vsnprintf(char *str, size_t size, const char *format, va_list ap){
         return 0;
     ret = vsnprintf(str, size, format, ap);
     // 如果输出被截断，保证字符串以 '\0' 结束
+    // if output is truncated, ensure string ends with '\0'
     if (ret >= (int)size) {
         str[size - 1] = '\0';
     }

363aafd371580d5361a1ed6712de49d4ea0d278e
Lucifer <gj3372819612@163.com>
2024-12-15 20:19:48 +0800

    add code

--- include/lu_hash_table-internal.h
+++ include/lu_hash_table-internal.h
@@ -16,14 +16,21 @@ typedef struct lu_hash_list_node_s {
     void*                       value;
 } lu_hash_list_node_t;
 
-typedef lu_hash_list_node_t lu_hash_list;
-typedef lu_hash_list_node_t element_t;
+typedef lu_hash_list_node_t *lu_hash_list_ptr_t;
+typedef lu_hash_list_node_t *element_ptr_t;
+
+//typedef ListNode *List;
+//typedef ListNode *Elemen
+
 
 typedef struct lu_hash_table_s {
-    int                         table_size;
-    lu_hash_list*               the_lists; 
+    int                               table_size;
+    lu_hash_list_ptr_t*               the_lists; 
 
 }lu_hash_table_t;
 
+
+int lu_Hash(int key,int table_size);
+lu_hash_table_t* luHashInit(int table_size);
 
 #endif /* LU_HASH_TABLE_INTERNAL_H */
---
+++ src/lu_hash_table.c
@@ -0,0 +1,36 @@
+#include "lu_hash_table-internal.h"
+#include <cstddef>
+#include <stdio.h>
+
+int lu_Hash(int key,int table_size)
+{    
+    return key % table_size;
+}
+
+lu_hash_table_t* luHashInit(int table_size){
+    int i = 0;
+    lu_hash_table_t* hash_table = NULL;
+    if(hash_table <= 0)
+        table_size = LU_HASH_TABLE_DEFAULT_SIZE;
+    hash_table = (lu_hash_table_t*)malloc(sizeof(lu_hash_table_t));
+    if(hash_table == NULL)
+    {
+        printf("lu_hash_table_init: malloc failed\n");
+        return NULL;
+    }
+    hash_table->table_size = table_size;
+
+    //为hash桶分配空间 其结构为一个数组指针
+    hash_table->the_lists = (lu_hash_list_ptr_t*)malloc(table_size * sizeof(lu_hash_list_ptr_t));
+    if(NULL == hash_table->the_lists){
+        printf("lu_hash_table_init: malloc the_lists failed\n");
+        free(hash_table);
+        return NULL;
+    }
+
+    //为 Hash 桶对应的指针数组初始化链表节点
+    for(i = 0; i < table_size; i++){
+        hash_table->the_lists[i] = (lu_hash_list_node_t *) malloc(sizeof(lu_hash_list_node_t));
+    }
+        
+}
